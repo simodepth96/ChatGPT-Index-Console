33b125cf4a57f88326fd6f6c4a9385f17cc1b1e9
Simone De Palma <135138011+simodepth96@users.noreply.github.com>
2025-11-29 18:08:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ if st.button("Check OpenAI Index", type="primary", disabled=not api_key or not u
                 elif (output_text.startswith("I can't access") or 
                       output_text.startswith("I couldn’t") or
                       output_text.startswith("I tried to open") or
-                      ",but " in output_text):
+                      ", but " in output_text):
                     st.warning("""
                     ⚠️ **ChatGPT-User may have been prevented by JavaScript blockers (CSR geo-location pop-ups e.g;) or the page simply couldn't be found in OpenAI's cached index.**
                     
